498cff443d33a3c9f2fabcbe2c9f1a3510405138
Yeabtsega Birhane <byeabtsega@yahoo.com>
2015-07-20 10:18:03 -0700

    Initial Commit

---
+++ GoodFlicks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoodFlicks
+//
+//  Created by Yeabtsega Birhane on 7/20/15.
+//  Copyright (c) 2015 Yeabtsega Birhane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
